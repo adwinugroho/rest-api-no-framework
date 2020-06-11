636538af3656567a5cb0fd93b3bf7f9827e8182d
adwin nugroho <adwinnugroho16@gmail.com>
2020-06-11 14:47:46 +0700

    add config (database) use arangodb

---
+++ config/init.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"log"
+
+	driver "github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver/http"
+)
+
+var instance *Connection
+
+type Connection struct {
+	DBLive driver.Client
+}
+
+func init() {
+	if instance != nil {
+		return
+	}
+
+	conn, err := http.NewConnection(http.ConnectionConfig{
+		Endpoints: []string{"http://localhost:8529"},
+	})
+	if err != nil {
+		log.Printf("Error open connection, cause:%+v\n", err)
+	}
+
+	client, err := driver.NewClient(driver.ClientConfig{
+		Connection: conn,
+	})
+	if err != nil {
+		log.Printf("Error create a new client connection, cause:%+v\n", err)
+	}
+
+	instance = &Connection{
+		DBLive: client,
+	}
+}
+
+// GetInstance return a connection instance
+func GetInstance() *Connection {
+	return instance
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module adwin/latihan/rest-no-framework
 
 go 1.13
 
-require golang.org/x/text v0.3.2 // indirect
+require (
+	github.com/arangodb/go-driver v0.0.0-20200609091535-b6946b6b973d
+	golang.org/x/text v0.3.2 // indirect
+)
